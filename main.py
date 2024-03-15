91690f64496c45cd97011424273d0c18f2b988bc
SiddD <108153549+siddz415@users.noreply.github.com>
2024-03-15 14:43:04 -0700

    added title

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from langchain_core.messages import AIMessage, HumanMessage
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+import langchain
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms.openai import ChatOpenAI
+
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
@@ -67,8 +70,8 @@ def get_response(user_input):
     return response['answer']
 
 # app config
-st.set_page_config(page_title="Chat with websites", page_icon="🤖")
-st.title("Chat with websites")
+st.set_page_config(page_title="Chat with AI", page_icon="🤖")
+st.title("Chat with AI")
 
 # sidebar
 with st.sidebar:
@@ -76,19 +79,19 @@ with st.sidebar:
     website_url = st.text_input("Website URL")
 
 if website_url is None or website_url == "":
-    st.info("Please enter a website URL")
+    st.info("Please enter a URL")
 
 else:
     # session state
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = [
-            AIMessage(content="Hello, I am a bot. How can I help you?"),
+            AIMessage(content="Hello, How can I help you?"),
         ]
     if "vector_store" not in st.session_state:
         st.session_state.vector_store = get_vectorstore_from_url(website_url)    
 
     # user input
-    user_query = st.chat_input("Type your message here...")
+    user_query = st.chat_input("Type here...")
     if user_query is not None and user_query != "":
         response = get_response(user_query)
         st.session_state.chat_history.append(HumanMessage(content=user_query))
